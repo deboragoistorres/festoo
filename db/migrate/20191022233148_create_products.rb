8bcbaa26d7b56dc7d9f0fce811286b61ee356113
Debora Torres <debora@torresetorres.com>
2019-10-22 20:36:52 -0300

    Add product model

---
+++ db/migrate/20191022233148_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :category
+      t.float :price
+      t.boolean :status, default: true
+
+      t.timestamps
+    end
+  end
+end
