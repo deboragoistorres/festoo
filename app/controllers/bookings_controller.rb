d503f0a5392a57e1b96fd9e3e49c931e87250b77
Vini Libero <libero.moreira@gmail.com>
2019-10-23 12:46:46 -0300

    controller for bookings created

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,7 @@
+class BookingsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/edit'
   devise_for :users
   root to: 'pages#home'
   resources :products
