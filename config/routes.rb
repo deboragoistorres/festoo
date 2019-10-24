5613b96591be89f4829542f5aa1a8b7de1402cb3
Gabriel Dornas <gabrielbdornas@gmail.com>
2019-10-24 20:18:17 -0300

    devise users controller and edit view translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   resources :products do
     resources :bookings
   end
