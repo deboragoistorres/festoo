89011300aa07f71831ca8477f1fac293ad4eff3a
Vini Libero <libero.moreira@gmail.com>
2019-10-24 21:01:00 -0300

    disappeared the form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+import flatpickr from 'flatpickr';
+
+require("flatpickr/dist/flatpickr.css");
+
+document.addEventListener("turbolinks:load", () => {
+  flatpickr(".datepicker", {
+    altInput: true,
+    altFormat: "d/m/Y",
+    dateFormat: "Y-m-d",
+    locale: Portuguese
+  });
