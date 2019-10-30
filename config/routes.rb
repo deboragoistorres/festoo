128e0012dc3041ef19a752428b1a4ec6959104b3
Gabriel Dornas <gabrielbdornas@gmail.com>
2019-10-30 19:17:00 -0300

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users, controllers: {
+    devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
 
