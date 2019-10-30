220fa7a39695bd73737b5e94aa49c05622519166
Vini Libero <libero.moreira@gmail.com>
2019-10-29 21:43:48 -0300

    made datepicker work

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
 gem 'puma'
 gem 'rails', '6.0.0'
+gem 'turbolinks'
 gem 'redis'
 
 gem 'autoprefixer-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,16 @@ import "bootstrap";
 import flatpickr from 'flatpickr';
 
 require("flatpickr/dist/flatpickr.css");
+console.log("applicationjs")
+
+
 
 document.addEventListener("turbolinks:load", () => {
+  console.log("turbolinks load")
   flatpickr(".datepicker", {
     altInput: true,
     altFormat: "d/m/Y",
     dateFormat: "Y-m-d",
-    locale: Portuguese
+    // locale: Portuguese
   });
 });
